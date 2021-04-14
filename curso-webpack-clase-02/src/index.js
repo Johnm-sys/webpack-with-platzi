1a74db13f61d038b5b57b134011296b5d45dfbba
Johnm-sys <jhonmenjura12@gmail.com>
2021-04-14 18:26:17 -0500

    fourth commit

--- curso-webpack-clase-02/src/index.js
+++ curso-webpack-clase-02/src/index.js
@@ -2,8 +2,6 @@ import Template from '@templates/Template.js';
 import '@style/main.css'
 import '@style/variables.scss'
 
-console.log('hello');
-
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
